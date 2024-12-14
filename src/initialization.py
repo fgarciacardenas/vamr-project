Rotation bug found, sort of works now

src/initialization.py
@@ -39,11 +39,12 @@ def initialize_vo(frame_manager, ft_params, klt_params, _debug: bool = False):
     F, mask = cv2.findFundamentalMat(points1=P_0_inliers, points2=P_2_inliers, method=cv2.FM_RANSAC)
 
     # Save outliers
-    P_0_outliers = np.concatenate([
-        P_0[matches_1_2.flatten() == 0],
-        P_1[matches_2_3.flatten() == 0],
-        P_0_inliers[mask.ravel() == 0]
-    ]).reshape([-1,2])
+    # P_0_outliers = np.concatenate([
+    #     P_0[matches_1_2.flatten() == 0],
+    #     P_1[matches_2_3.flatten() == 0],
+    #     P_0_inliers[mask.ravel() == 0]
+    # ]).reshape([-1,2])
+    P_0_outliers = None
 
     # Select inlier points
     P_0_inliers = P_0_inliers[mask.ravel() == 1]

src/run.py
@@ -9,7 +9,7 @@ DEBUG = False
 
 def main():
     # Initialize FrameManager
-    dataset_dir = {'kitty': 0, 'malaga': 1, 'parking': 2}
+    dataset_dir = {'kitti': 0, 'malaga': 1, 'parking': 2}
     frame_manager = FrameManager(base_path='/home/dev/data', dataset=dataset_dir[DATASET], bootstrap_frames=[0, 1])
     
     # Load K matrix
@@ -89,11 +89,8 @@ def main():
         P_0_outliers = np.concatenate([previous_state['keypoints_2D'][matches == 0], P_0_inliers[np.setdiff1d(np.arange(len(P_0_inliers)), rs_inliers)]])
         P_0_inliers = P_0_inliers[rs_inliers]
 
-        # Compute camera poses in the world frame
-        previous_pose = pose_arr[-1]
-        current_transformation = np.vstack((np.hstack((R, t)),
-                                            np.array([0,0,0,1])))
-        next_pose = np.linalg.inv(current_transformation)@previous_pose
+        next_pose = np.vstack((np.hstack((R, t)),
+                               np.array([0,0,0,1])))
         pose_arr.append(next_pose)
 
         # Compute feature candidates
@@ -187,7 +184,7 @@ def main():
         visualizer.update_image(I_curr)
         visualizer.update_plot(iFrame)
         iFrame += 1
-        if iFrame >= 100:
+        if iFrame >= 50:
             break
 
     visualizer.close_video()